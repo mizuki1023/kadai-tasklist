f826e9ad43510f2dc5d0214237258467d195d472
apple <apple@192.168.3.10>
2024-04-16 21:47:54 +0900

    テーブル自動作成成功

--- src/models/Task.java
+++ src/models/Task.java
@@ -7,10 +7,20 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
-
 @Entity
-@Table(name = "task")
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllTasks",
+                        //Taskはクラス名　
+        query = "SELECT m FROM Task AS m ORDER BY m.id DESC"
+    )
+})
+
+//tasks = テーブル名
+@Table(name = "tasks")
 public class Task {
     @Id
     @Column(name = "id")
